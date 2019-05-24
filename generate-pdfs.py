b08231fd75d8e234deaecb97ed7459edecc2f999
Daniel P <pawlik.dan@gmail.com>
2019-05-24 12:45:52 +0200

    Added merging pdf

--- generate-pdfs.py
+++ generate-pdfs.py
@@ -1,5 +1,9 @@
 #!/usr/local/env python3
 
+import glob
+import os
+
+from PyPDF2 import PdfFileMerger
 from weasyprint import HTML, CSS
 
 cssStr = """
@@ -13,9 +17,19 @@ cssStr = """
 #pre-footer, footer, header, .feedback--prompt, .feedback--yes, .feedback--no,
 #hero, #user-journeys-toc, #content .track,
 .docssectionheaders:last-child, .docssectionheaders:last-of-type,
-.sections, #editPageButton, #docsToc, #feedback, #feedback--prompt, #what-s-next
+.sections, #editPageButton, #docsToc, #feedback, #feedback--prompt
 {
     display: none !important;
+}
+
+.feedback
+{
+    display: none !important;
+}
+
+h1 {
+    margin:0;
+    font-size 10;
 }
 
 #docsContent {
@@ -28,21 +42,46 @@ cssStr = """
 }
 """
 
+
 def get_sites():
     sites = []
     with open('./k8s-sites', 'r') as f:
-      for line in f:
-         sites.append(line)
+        for line in f:
+            sites.append(line)
     return sites
 
+
 def generate_site(sites):
+    section_order = []
     for site in sites:
-        name = site.rsplit('/',2)[1]
-        print(name)
+        name = site.rsplit('/', 2)[1]
         filename = "%s.pdf" % name
+        print("Generating %s" % name)
         HTML(url=site).write_pdf(filename, stylesheets=[CSS(string=cssStr)])
+        section_order.append(name)
+    return section_order
+
+
+def get_pdf_files(section_order):
+    pdf_files = []
+    for f in section_order:
+        name = "%s.pdf" % f
+        if glob.glob(name):
+            pdf_files.append(name)
+    return pdf_files
+
+
+def merge_files(pdf_files):
+    merger = PdfFileMerger()
+    for pdf in pdf_files:
+        merger.append(open(pdf, 'rb'))
+
+    with open('k8s-book.pdf', 'wb') as fout:
+        merger.write(fout)
 
 
 if __name__ == '__main__':
     sites = get_sites()
-    generate_site(sites)
+    section_order = generate_site(sites)
+    pdf_files = get_pdf_files(section_order)
+    merge_files(pdf_files)
