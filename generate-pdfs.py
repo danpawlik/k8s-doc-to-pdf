fa82e60d2ae8fa88cb48cf85a8577982f6886db0
Daniel P <pawlik.dan@gmail.com>
2019-05-30 16:04:17 +0200

    Changed CSS style

--- generate-pdfs.py
+++ generate-pdfs.py
@@ -2,41 +2,47 @@
 
 import glob
 import os
+import cssutils
 
 from PyPDF2 import PdfFileMerger
 from weasyprint import HTML, CSS
 
 cssStr = """
-@page {
-  size: Letter;
-  margin: 0.8cm;
+
+#docsContent h1, #docsContent h2, #docsContent h3, #docsContent h4,
+#docsContent h5, #docsContent h6 {
+    line-height: 0.4 !important;
+    font-weight: 500 !important;
+    margin-bottom: 10px !important;
+    padding-bottom: 0 !important;
 }
 
-@font-size 10;
+#docsContent pre {
+    margin-top: 0px !important;
+    margin-bottom: 5px !important;
+    padding: 10px !important;
+}
+
+@page {
+  size: Letter !important;
+  margin: 0.5cm !important;
+}
+
+#docsContent h1, #docsContent h2 {
+    border-bottom-style: none !important;
+}
 
 #pre-footer, footer, header, .feedback--prompt, .feedback--yes, .feedback--no,
-#hero, #user-journeys-toc, #content .track,
+#hero, #user-journeys-toc, #content .track, .what-s-next,
 .docssectionheaders:last-child, .docssectionheaders:last-of-type,
 .sections, #editPageButton, #docsToc, #feedback, #feedback--prompt
 {
     display: none !important;
 }
-
-.feedback
-{
-    display: none !important;
-}
-
-h1 {
-    margin:0;
-    font-size 10;
-}
-
 #docsContent {
     float: none !important;
     width: 100% !important;
 }
-
 #encyclopedia {
     padding-bottom: 0 !important;
 }
@@ -80,8 +86,17 @@ def merge_files(pdf_files):
         merger.write(fout)
 
 
+def css_validation():
+    parsed_css = cssutils.parseString(cssStr)
+    return parsed_css.valid
+
+
 if __name__ == '__main__':
     sites = get_sites()
+
+    if not css_validation():
+        print("Your CSS is not valid!")
+
     section_order = generate_site(sites)
     pdf_files = get_pdf_files(section_order)
     merge_files(pdf_files)
